5e0555a8896638faf7eaec8d954c5838e0402f3e
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-08-20 19:19:53 -0400

    Return null if user not found and adjust JOIN query

--- db/query.js
+++ db/query.js
@@ -3,11 +3,17 @@ const pool = require("./pool");
 class User {
    async getUserByEmail(email) {
       const { rows } = await pool.query("SELECT * FROM user_account WHERE email=$1", [email]);
-      return rows[0];
+      return rows[0] || null;
    }
    async getUserById(id) {
       const { rows } = await pool.query("SELECT * FROM user_account WHERE id=$1", [id]);
-      return rows[0];
+      return rows[0] || null;
+   }
+
+   async createUser({ email, password, firstname, lastname }) {
+      const query =
+         "INSERT INTO user_account (email, password, firstname, lastname) VALUES ($1, $2, $3, $4)";
+      await pool.query(query);
    }
 }
 
@@ -16,10 +22,10 @@ class Message {
       const query = `
         SELECT *
         FROM user_account 
+        LEFT JOIN user_role ON user_account.id = user_role.user_account_id 
+        LEFT JOIN role ON user_role.role_id = role.id
         JOIN user_message ON user_account.id = user_message.user_account_id
-        JOIN message ON user_message.message_id = message.id 
-        JOIN user_role ON user_account.id = user_role.user_account_id 
-        JOIN role ON user_role.role_id = role.id`;
+        JOIN message ON user_message.message_id = message.id`;
       const { rows } = await pool.query(query);
 
       return rows;
