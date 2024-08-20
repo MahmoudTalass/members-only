442391fb5fb97db7306eba5ec9e8550ea0e3ed47
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-08-20 17:39:32 -0400

    Create and export function to set up passport local strategy

---
+++ auth/auth.js
@@ -0,0 +1,42 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { User } = require("../db/query");
+const bcrypt = require("bcryptjs");
+
+module.exports.setUpAuth = (app) => {
+   app.use(passport.session());
+   passport.use(
+      new LocalStrategy({ usernameField: "email" }, async (username, password, done) => {
+         try {
+            const user = await User.getUser(username);
+
+            if (!user) {
+               return done(null, false, { message: "Incorrect username" });
+            }
+
+            const matched = await bcrypt.compare(password, user.password);
+
+            if (!matched) {
+               return done(null, false, { message: "Incorrect password" });
+            }
+
+            return done(null, user);
+         } catch (err) {
+            done(err);
+         }
+      })
+   );
+
+   passport.serializeUser((user, done) => {
+      done(null, user.id);
+   });
+
+   passport.deserializeUser(async (id, done) => {
+      try {
+         const user = await User.getUserById(id);
+         done(null, user);
+      } catch (err) {
+         done(err);
+      }
+   });
+};
