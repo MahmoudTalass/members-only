587b152ebd14cbd375b99c9eebae9f4eae06be18
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-08-21 20:46:12 -0400

    Add logout functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -57,9 +57,17 @@ const loginUserPost = [
    },
 ];
 
+const logoutPost = (req, res, next) => {
+   req.logout((err) => {
+      if (err) return next(err);
+      res.redirect("/");
+   });
+};
+
 module.exports = {
    signUpUserGet,
    signUpUserPost,
    loginUserPost,
    loginUserGet,
+   logoutPost,
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,6 +5,8 @@ const authController = require("../controllers/auth.controller");
 router.get("/login", authController.loginUserGet);
 router.post("/login", authController.loginUserPost);
 
+router.post("/logout", authController.logoutPost);
+
 router.get("/sign-up", authController.signUpUserGet);
 router.post("/sign-up", authController.signUpUserPost);
 
